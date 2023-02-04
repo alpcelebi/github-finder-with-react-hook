295de665a61da1d652eaf4b1de5844bb19456e08
“sadikturan” <sadikturan@gmail.com>
2023-02-04 23:58:33 +0300

    7_1_statefuull-function-component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,49 +1,38 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 
-export class Search extends Component {
+const Search = ({ displayAlert,searchUsers,showClearButton,clearResults }) => {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            keyword: ''
-        }
+    const [keyword, setKeyword] = useState('');
+
+    const onChange = (e) => {
+        setKeyword(e.target.value);
     }
 
-    onChange = (e) => {
-        this.setState({
-            keyword: e.target.value
-        })
-    }
-
-    onSubmit = (e) => {
+    const onSubmit = (e) => {
         e.preventDefault();
 
-        if(this.state.keyword === '') {
-            this.props.displayAlert('Anahtar Kelime Giriniz', 'warning');
+        if(keyword === '') {
+            displayAlert('Anahtar Kelime Giriniz', 'warning');
         } else {
-            this.props.searchUsers(this.state.keyword);
-            this.setState({ keyword: '' });
+            searchUsers(keyword);
+            setKeyword('');
         }
     }
 
-    render() {
-        return (
+    return (
         <div className="container my-3">
-            <form onSubmit={this.onSubmit}>
+            <form onSubmit={onSubmit}>
                 <div className="input-group">
-                    <input type="text" value={this.state.keyword} onChange={this.onChange} className='form-control' placeholder='Anahtar Kelime' />
+                    <input type="text" value={keyword} onChange={onChange} className='form-control' placeholder='Anahtar Kelime' />
                     <button className="btn btn-primary" type='submit'>Ara</button>
                 </div>
             </form>
-
             {
-                this.props.showClearButton && <button onClick={this.props.clearResults} className="btn btn-outline-danger mt-2 btn-block">Sonuçları Temizle</button>
-            }
-           
+                showClearButton && <button onClick={clearResults} className="btn btn-outline-danger mt-2 btn-block">Sonuçları Temizle</button>
+            }        
 
         </div>
-        )
-    }
+    )
 }
 
 export default Search
